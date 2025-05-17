0a14fd65e6da6e148b4ebfb5adecb590d6f6d456
Sourabh Kumar <109268566+Sourabh1789101@users.noreply.github.com>
2025-05-17 23:24:13 +0530

    Time: 350 ms (87.34%) | Memory: 0B (100.00%) - LeetSync

--- 1670-patients-with-a-condition/patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/patients-with-a-condition.sql
@@ -1,3 +1,6 @@
-# Write your MySQL query statement below
-SELECT * FROM Patients
-Where conditions like 'DIAB1%' or conditions like '% DIAB1%';
+SELECT patient_id, patient_name, conditions
+FROM Patients
+WHERE CONCAT(' ', conditions, ' ') LIKE '% DIAB1%';
+
+
+
