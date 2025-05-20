8b85fd1195914bc9faa34e23cb43edb8b7292164
Sourabh Kumar <109268566+Sourabh1789101@users.noreply.github.com>
2025-05-20 08:30:49 +0530

    Time: 585 ms (94.66%) | Memory: 0B (100.00%) - LeetSync

--- 1811-fix-names-in-a-table/fix-names-in-a-table.sql
+++ 1811-fix-names-in-a-table/fix-names-in-a-table.sql
@@ -3,10 +3,3 @@ SELECT user_id,
        CONCAT(Upper(Left(name,1)),lower(substr(name,2))) as name
 FROM Users
 Order by User_id;
-
-
-
-#select user_id, 
-#   concat(upper(left(name,1)),lower(substring(name,2))) as name
-#from Users 
-#order by user_id
